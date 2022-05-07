45a2698711b604207c0a56b33d072ae4ae316035
Chervatin <jmchervatin@gmail.com>
2022-05-07 17:34:06 +0200

    Exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("") // Split breaks the string and makes and array of its elements. Reverse will reverse the array and then join will convert the array into a string again 
 };
 
 // Do not edit below this line
