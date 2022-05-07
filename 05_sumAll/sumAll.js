e752cc1ffba4e4284b4447e640a6c7a6e3a0a4c9
Chervatin <jmchervatin@gmail.com>
2022-05-07 19:16:42 +0200

    Exercise sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
 
+    var difBetweenInt = int2-int1;
+    var sumBetweenInt = int1
+
+if(typeof int1 == 'number' && typeof int2 == 'number' && int1>=0 && int2>=0){
+
+if (int2<int1){
+    var difBetweenInt = int1-int2;
+    var sumBetweenInt = int2;
+}
+
+console.log(difBetweenInt)
+
+for (let i = 1; i <= difBetweenInt ; i++) {
+    sumBetweenInt = sumBetweenInt + (i+1)
+}
+
+return sumBetweenInt;
+
+}
+
+else
+
+return 'ERROR'
 };
 
 // Do not edit below this line
