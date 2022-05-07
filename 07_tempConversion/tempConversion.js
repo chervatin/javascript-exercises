05da40e48136d78724d0221787930bcb7be002bb
Chervatin <jmchervatin@gmail.com>
2022-05-07 19:49:33 +0200

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
+let celsius = (fahr-32)*(5/9);
+return parseFloat((Math.round(celsius * 10) / 10).toFixed(1))
 
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+fahrenheit = cels*(9/5) + 32;
+return parseFloat((Math.round(fahrenheit * 10) / 10).toFixed(1))
 };
 
 // Do not edit below this line
