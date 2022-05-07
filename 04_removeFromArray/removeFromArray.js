5cbdec9d1619b651bd4c2612947f22c7b09066cf
Chervatin <jmchervatin@gmail.com>
2022-05-07 18:28:39 +0200

    Exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...remove) {
+    let arrayRemove = remove;
+    let removeLength = arrayRemove.length;
+    
+    for (let i = 0; i < removeLength; i++) {
+        const locationToRemove = array.indexOf(arrayRemove[i]);
+        if (locationToRemove >= 0){
+        array.splice(locationToRemove, 1);
+        } 
+    }
+   
+    return array;
 };
 
 // Do not edit below this line
