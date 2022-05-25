b2ae0079cbdf14045c293ae51843c6c92243cd6d
Chervatin <jmchervatin@gmail.com>
2022-05-25 20:54:18 +0200

    Calculator solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
+  
 };
 
-const subtract = function() {
-	
+const subtract = function(c,d) {
+	return c-d
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current,0);
+ }
+
+
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return a**b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  if (a==0) { 
+    return 1 
+  } 
+  else {
+  let result = a;
+  for (let index = a-1; index >0; index--) {
+    result = result * index
+  }
+  return result;
+}
+}
 
 // Do not edit below this line
 module.exports = {
