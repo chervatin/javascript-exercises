d7f0096fce8e9d5775d0d5be7c4fb83462825d9f
Chervatin <jmchervatin@gmail.com>
2022-05-27 16:42:52 +0200

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+
+
+let series = [1, 1];
+let number = input;
+if (number>0){
+    for (let index = 1; index < number; index++) {
+        series.push(series[index] + series[index-1]);
+    }
+} else 
+return 'OOPS'
+
+return series[input-1];
+
+
+//1 1 2 3 5 8 
 
 };
 
