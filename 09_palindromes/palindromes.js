7983dacd983b3a32961a8813d39e3a73746c88e5
Chervatin <jmchervatin@gmail.com>
2022-05-27 14:49:06 +0200

    Finish palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const modString = string.replaceAll(/[^a-zA-Z0-9]+/g, "");
+    const modString2 = modString.replaceAll(" ", "");
+    const modString3 = modString2.toLowerCase();
+    console.log(modString3);
+    var splitString = modString3.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return (modString3 === joinArray);
+  
 };
 
 // Do not edit below this line
