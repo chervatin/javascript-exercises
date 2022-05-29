ff0635d5f0d364c7852aa9dc41dd1c7b6b14b559
Chervatin <jmchervatin@gmail.com>
2022-05-29 19:30:11 +0200

    getTheTitles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let result = obj.map(a => a.title); 
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
